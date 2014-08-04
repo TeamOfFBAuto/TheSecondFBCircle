9b95cf70ad88a86691da2573d3d5ad69614b9d47
fblife-zhang <zhangshaonan@fblife.com>
2014-08-04 16:27:23 +0800

    zsn
    zsn

--- FBCircle/Message/MessageControllers/MessageViewController.h
+++ FBCircle/Message/MessageControllers/MessageViewController.h
@@ -2,12 +2,34 @@
 //  MessageViewController.h
 //  FBCircle
 //
-//  Created by soulnear on 14-8-4.
-//  Copyright (c) 2014年 soulnear. All rights reserved.
+//  Created by soulnear on 14-5-19.
+//  Copyright (c) 2014年 szk. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "MessageModel.h"
+#import "CustomMessageCell.h"
+#import "ChatViewController.h"
+#import "GmyMessageViewController.h"
 
-@interface MessageViewController : UIViewController
+@interface MessageViewController : MyViewController<UITableViewDelegate,UITableViewDataSource>
+{
+    UIImageView *    _tixing_label ;
+    
+    BOOL isnewfbnotification;
+    
+    NSTimer * theTimer;
+}
+
+@property(nonatomic,strong)UITableView * myTableView;
+
+@property(nonatomic,strong)MessageModel * theModel;
+
+@property(nonatomic,strong)NSMutableArray * data_array;
+
+@property(nonatomic,strong)NSDictionary * system_notification_dictionary;
+
+
+
 
 @end
